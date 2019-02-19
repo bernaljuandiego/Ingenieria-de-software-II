16473f81ee4a592f0b6d142e8240580ce8b4d9a1
BryanPinzon98 <bryans.pinzonc@gmail.com>
2019-02-19 14:32:45 -0500

    Create Register Activity and Handler
    It has been created Register Activity and handler into Login Activity. Register view is empty still.

--- excolnet/app/src/main/java/co/edu/konradlorenz/excolnet/LoginActivity.java
+++ excolnet/app/src/main/java/co/edu/konradlorenz/excolnet/LoginActivity.java
@@ -3,6 +3,7 @@ package co.edu.konradlorenz.excolnet;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -55,6 +56,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
+    private TextView createAccountText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,6 +90,21 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+        //No account yet button text hanlder
+        createAccountText = findViewById(R.id.createAccount);
+        createAccountText.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openRegisterView();
+            }
+        });
+    }
+
+    //This method open register activity
+    public void openRegisterView(){
+        Intent newIntent = new Intent(this, RegisterActivity.class);
+        startActivity(newIntent);
     }
 
     private void populateAutoComplete() {
