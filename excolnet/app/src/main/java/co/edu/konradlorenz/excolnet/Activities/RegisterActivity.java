af3a6caf2bd1dbb1d5f9d8cd5803223af7be5d5b
BryanPinzon <drums830@gmail.com>
2019-02-20 14:21:57 -0500

    Visual Components of Register Module.
    Layout is complete.

--- excolnet/app/src/main/java/co/edu/konradlorenz/excolnet/Activities/RegisterActivity.java
+++ excolnet/app/src/main/java/co/edu/konradlorenz/excolnet/Activities/RegisterActivity.java
@@ -1,15 +1,58 @@
 package co.edu.konradlorenz.excolnet.Activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 import co.edu.konradlorenz.excolnet.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private static final int PICK_IMAGE_REQUEST = 100;
+    private ImageView imageRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        getLayoutComponents();
+    }
+
+    public void getLayoutComponents(){
+        imageRegister = findViewById(R.id.image_selected_view);
+    }
+
+    public void searchImage(View v){
+        Intent newIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        newIntent.setType("image/*");
+        startActivityForResult(newIntent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode){
+            case PICK_IMAGE_REQUEST:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = data.getData();
+
+                    try{
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                        imageRegister.setImageBitmap(bitmap);
+                    }catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+                break;
+        }
     }
 }
