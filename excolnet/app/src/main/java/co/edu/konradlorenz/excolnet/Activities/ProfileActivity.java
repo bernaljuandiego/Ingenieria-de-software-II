22d536ad8097611fc2e7f1a21dd65a580fe1da28
BryanPinzon <drums830@gmail.com>
2019-03-23 07:33:00 -0500

    Profile layout was added.

---
+++ excolnet/app/src/main/java/co/edu/konradlorenz/excolnet/Activities/ProfileActivity.java
@@ -0,0 +1,16 @@
+package co.edu.konradlorenz.excolnet.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import co.edu.konradlorenz.excolnet.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
+}
