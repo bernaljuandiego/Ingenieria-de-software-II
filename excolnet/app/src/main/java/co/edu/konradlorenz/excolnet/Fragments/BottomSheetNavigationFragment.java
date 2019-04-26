c3a284eeab8ba36ea87ac19e1fb9907ea4e8beac
Enrique suarez <enriquesuarezgtte@gmail.com>
2019-04-25 21:19:41 -0500

    Fragment

--- excolnet/app/src/main/java/co/edu/konradlorenz/excolnet/Fragments/BottomSheetNavigationFragment.java
+++ excolnet/app/src/main/java/co/edu/konradlorenz/excolnet/Fragments/BottomSheetNavigationFragment.java
@@ -18,6 +18,9 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import co.edu.konradlorenz.excolnet.Activities.LoginActivity;
 import co.edu.konradlorenz.excolnet.R;
 
@@ -92,7 +95,13 @@ public class BottomSheetNavigationFragment extends BottomSheetDialogFragment {
                         return true;
                     case R.id.nav_tourist_sites_option:
                         Toast.makeText(getContext(), "Tourist Sites Option Selected", Toast.LENGTH_SHORT).show();
-                        return true;
+
+                        Fragment fragment = new TouristSitesFragment();
+                        FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+                        ft.replace(R.id.contenido,fragment);
+                        ft.addToBackStack(null);
+                        ft.commit();
+                        BottomSheetNavigationFragment.super.dismiss();
                     case R.id.nav_settings_option:
                         Toast.makeText(getContext(), "Settings Option Selected", Toast.LENGTH_SHORT).show();
                         return true;
---
+++ excolnet/app/src/main/java/co/edu/konradlorenz/excolnet/Fragments/TouristSitesFragment.java
@@ -0,0 +1,40 @@
+package co.edu.konradlorenz.excolnet.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.edu.konradlorenz.excolnet.R;
+
+
+public class TouristSitesFragment extends Fragment {
+
+
+
+    public TouristSitesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tourist_sites, container, false);
+    }
+
+}
