17be7d903778101c79001c394d56dcd382107c13
Enrique Suarez <enriquesuarezgtte@gmail.com>
2019-05-17 09:43:32 -0500

    Adapter working

--- excolnet/app/src/main/java/co/edu/konradlorenz/excolnet/Activities/LivingCostActivity.java
+++ excolnet/app/src/main/java/co/edu/konradlorenz/excolnet/Activities/LivingCostActivity.java
@@ -1,7 +1,10 @@
 package co.edu.konradlorenz.excolnet.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
@@ -10,6 +13,7 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.List;
 
+import co.edu.konradlorenz.excolnet.Adapters.AdapterPrecios;
 import co.edu.konradlorenz.excolnet.Entities.Ciudad;
 import co.edu.konradlorenz.excolnet.Entities.Precios;
 import co.edu.konradlorenz.excolnet.Interfaces.PriceService;
@@ -21,20 +25,32 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LivingCostActivity extends AppCompatActivity {
-    ListView listView;
-    ArrayList<String> precios;
-    ArrayAdapter arrayAdapter;
+    RecyclerView recyclerView;
+    private RecyclerView.Adapter adapterPrecios;
+    private RecyclerView.LayoutManager mlayoutManagerS;
+
     List<Precios> listaDePreciosEntity;
+    List<String> listaNombrPrecios;
+    ArrayList<String> listaPrecios;
+    Context context;
+    private Ciudad ciudad;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_living_cost);
-        precios = new ArrayList<>();
-        arrayAdapter = new ArrayAdapter(this, R.layout.precios, precios);
-        listView=findViewById(R.id.precios);
-        listaDePreciosEntity = new ArrayList<Precios>();
-        listView.setAdapter(arrayAdapter);
+        context=this;
+        listaDePreciosEntity = new ArrayList<>();
+        listaNombrPrecios=new ArrayList<>();
+        listaPrecios=new ArrayList<>();
+        ciudad = new Ciudad();
+       //  arrayAdapter = new ArrayAdapter(this, R.layout.precios,listaPrecios);
+        adapterPrecios = new AdapterPrecios(context,listaDePreciosEntity);
+        recyclerView=findViewById(R.id.preciosrecycler);
+
+        recyclerView.setAdapter(adapterPrecios);
         getCiudad();
+
 
 
     }
@@ -45,18 +61,28 @@ public class LivingCostActivity extends AppCompatActivity {
         Retrofit retrofit = new Retrofit.Builder().baseUrl("https://www.numbeo.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+
         PriceService priceService = retrofit.create(PriceService.class);
         Call<Ciudad> call = priceService.getCiudad();
         call.enqueue(new Callback<Ciudad>() {
             @Override
             public void onResponse(Call<Ciudad> call, Response<Ciudad> response) {
 
-                    listaDePreciosEntity= response.body().getPrices();
-                for(Precios str:listaDePreciosEntity){
-                    precios.add(str.getItem_name());
-                }
-                arrayAdapter.notifyDataSetChanged();
+                ciudad = response.body();
 
+               Log.w("LIVING COST", ciudad.getPrices().toString());
+               listaDePreciosEntity = ciudad.getPrices();
+
+               for (Precios list: ciudad.getPrices()){
+
+                   listaPrecios.add(list.getItem_name());
+               }
+                recyclerView =(RecyclerView) findViewById(R.id.preciosrecycler);
+                recyclerView.setHasFixedSize(true);
+                mlayoutManagerS = new LinearLayoutManager(context);
+                recyclerView.setLayoutManager(mlayoutManagerS);
+                adapterPrecios = new AdapterPrecios(context,listaDePreciosEntity);
+                recyclerView.setAdapter(adapterPrecios);
             }
 
             @Override
@@ -65,4 +91,5 @@ public class LivingCostActivity extends AppCompatActivity {
             }
         });
     }
+
 }
--- excolnet/app/src/main/java/co/edu/konradlorenz/excolnet/Adapters/AdapterPrecios.java
+++ excolnet/app/src/main/java/co/edu/konradlorenz/excolnet/Adapters/AdapterPrecios.java
@@ -1,4 +1,66 @@
 package co.edu.konradlorenz.excolnet.Adapters;
 
-public class AdapterPrecios {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.alespero.expandablecardview.ExpandableCardView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import co.edu.konradlorenz.excolnet.Entities.Precios;
+import co.edu.konradlorenz.excolnet.R;
+
+public class AdapterPrecios extends RecyclerView.Adapter<AdapterPrecios.PriceHolder> {
+        private Context mContext;
+        List<Precios> precios_items;
+        private View view;
+        ExpandableCardView cardView;
+
+    public AdapterPrecios(Context mContext, List<Precios> precios_items) {
+        this.mContext = mContext;
+        this.precios_items = precios_items;
+    }
+
+    @NonNull
+    @Override
+    public PriceHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        view=LayoutInflater.from(parent.getContext()).inflate(R.layout.livingcard_layout,parent,false);
+        findMaterialElements();
+        PriceHolder priceHolder = new PriceHolder(view);
+        return priceHolder;
+    }
+
+    private void findMaterialElements() {
+       cardView=view.findViewById(R.id.expand_card);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PriceHolder holder, int position) {
+        holder.expandableCardView.setTitle(precios_items.get(position).getItem_name());
+        holder.textView.setText(precios_items.get(position).getAverage_price()+"");
+    }
+
+    @Override
+    public int getItemCount() {
+        return precios_items.size();
+    }
+
+
+    public class PriceHolder extends RecyclerView.ViewHolder{
+        ExpandableCardView expandableCardView;
+        TextView textView;
+
+        public PriceHolder(@NonNull View itemView) {
+            super(itemView);
+            expandableCardView =itemView.findViewById(R.id.expand_card);
+            textView = itemView.findViewById(R.id.precios_text);
+        }
+    }
 }
