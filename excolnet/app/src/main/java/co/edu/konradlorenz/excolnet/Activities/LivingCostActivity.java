702abfb942620586f117876ccdd634bca17d6729
Enrique Suarez <enriquesuarezgtte@gmail.com>
2019-05-17 16:59:38 -0500

    Selected list working

--- excolnet/app/src/main/java/co/edu/konradlorenz/excolnet/Activities/LivingCostActivity.java
+++ excolnet/app/src/main/java/co/edu/konradlorenz/excolnet/Activities/LivingCostActivity.java
@@ -1,14 +1,18 @@
 package co.edu.konradlorenz.excolnet.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,19 +35,29 @@ public class LivingCostActivity extends AppCompatActivity {
 
     List<Precios> listaDePreciosEntity;
     List<String> listaNombrPrecios;
-    ArrayList<String> listaPrecios;
-    Context context;
+    private ArrayList<String> listaPrecios;
+    private Context context;
     private Ciudad ciudad;
-
+    private SearchView searchView;
+    private Spinner spinner;
+    private ArrayAdapter adapterSpinner;
+    private String selectedItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_living_cost);
         context=this;
+        selectedItem="Bogota";
         listaDePreciosEntity = new ArrayList<>();
         listaNombrPrecios=new ArrayList<>();
         listaPrecios=new ArrayList<>();
         ciudad = new Ciudad();
+        searchView = findViewById(R.id.search_cost);
+        spinner = findViewById(R.id.spinner);
+
+        adapterSpinner=ArrayAdapter.createFromResource(this,R.array.city_list,R.layout.item_spinner);
+        adapterSpinner.setDropDownViewResource(R.layout.item_spinner);
+        spinner.setAdapter(adapterSpinner);
        //  arrayAdapter = new ArrayAdapter(this, R.layout.precios,listaPrecios);
         adapterPrecios = new AdapterPrecios(context,listaDePreciosEntity);
         recyclerView=findViewById(R.id.preciosrecycler);
@@ -63,14 +77,13 @@ public class LivingCostActivity extends AppCompatActivity {
                 .build();
 
         PriceService priceService = retrofit.create(PriceService.class);
-        Call<Ciudad> call = priceService.getCiudad();
+        Call<Ciudad> call = priceService.getCiudad("hfkibcktwtxi4f",selectedItem);
         call.enqueue(new Callback<Ciudad>() {
             @Override
             public void onResponse(Call<Ciudad> call, Response<Ciudad> response) {
 
                 ciudad = response.body();
-
-               Log.w("LIVING COST", ciudad.getPrices().toString());
+//https://www.numbeo.com/api/city_prices?api_key%3Dhfkibcktwtxi4f%26query=Bogota
                listaDePreciosEntity = ciudad.getPrices();
 
                 recyclerView =(RecyclerView) findViewById(R.id.preciosrecycler);
@@ -88,4 +101,59 @@ public class LivingCostActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(searchView!=null){
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    search(newText);
+                    return true;
+                }
+            });
+
+        }
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedItem=spinner.getSelectedItem().toString();
+                getCiudad();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                selectedItem =spinner.getSelectedItem().toString();
+                adapterPrecios.notifyDataSetChanged();
+            }
+        });
+
+    }
+    private void search (String newText){
+        ArrayList<Precios> precios = new ArrayList<>();
+        for(Precios pr: listaDePreciosEntity){
+            if(pr.getItem_name().toLowerCase().contains(newText.toLowerCase())
+            &&!newText.equals(" ")){
+                precios.add(pr);
+            }
+        }
+
+        recyclerView =(RecyclerView) findViewById(R.id.preciosrecycler);
+        recyclerView.setHasFixedSize(true);
+        mlayoutManagerS = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(mlayoutManagerS);
+        adapterPrecios = new AdapterPrecios(context,precios);
+        recyclerView.setAdapter(adapterPrecios);
+adapterPrecios.notifyDataSetChanged();
+
+
+
+
+    }
 }
--- excolnet/app/src/main/java/co/edu/konradlorenz/excolnet/Interfaces/PriceService.java
+++ excolnet/app/src/main/java/co/edu/konradlorenz/excolnet/Interfaces/PriceService.java
@@ -5,10 +5,13 @@ import java.util.List;
 import co.edu.konradlorenz.excolnet.Entities.Ciudad;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface PriceService {
-    String API_ROUTE = "/api/city_prices?api_key=hfkibcktwtxi4f&query=Bogota";
+    String API_ROUTE = "/api/city_prices?api_key=hfkibcktwtxi4f&query=";
 
-    @GET(API_ROUTE)
-    Call < Ciudad> getCiudad();
+    @GET("/api/city_prices")
+    Call < Ciudad> getCiudad(@Query("api_key") String api, @Query("query") String query);
+
 }
